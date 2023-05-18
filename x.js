c3ab88536b39bf327411e1dc92c8ac41c89932db
Reedam Ranjan <reedamranjan@gmail.com>
2023-05-18 12:24:54 +0530

    first commit

---
+++ x.js
@@ -0,0 +1,14 @@
+let arr=[1,2,1,2,3,4,4,1]
+// expected output
+// {
+//     1:2,
+//     2:2,
+//     3:1,
+//     4:2
+// }
+
+const frequencyCount = (inputArray) => {
+
+}
+
+console.log(frequencyCount(arr))
